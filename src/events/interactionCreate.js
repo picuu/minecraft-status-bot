21cc1da7ce9c501088a91c28c0827155611d6e5c
Picuu <ltpicuuu@gmail.com>
2022-08-02 23:23:30 +0200

    remove buttons & selectMenus

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,13 +1,11 @@
 const executeCommand = require("../functions/executeCommand.js");
-const executeSelectMenu = require("../functions/executeSelectMenu.js");
-const executeButton = require("../functions/executeButton.js");
 
 module.exports = {
     name: "interactionCreate",
     
     async execute(client, interaction) {
         
-        if (interaction.isCommand()) executeCommand(client, interaction);
+        if (interaction.isChatInputCommand()) executeCommand(client, interaction);
 
     }
 }
